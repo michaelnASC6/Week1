2f574d5900cfa351580dce1aa5329c60ed2c9b6b
michaelnASC6 <mnewby0503@gmail.com>
2019-07-15 13:55:58 -0400

    Beginning of larger tasks

--- day0/marioPaintMVP/sketch.js
+++ day0/marioPaintMVP/sketch.js
@@ -7,18 +7,11 @@ let value = 0;
 function draw(){
     fill(value);
     rect(25, 25, 50, 50);
+    triangle(150, 40, 200, 100, 100, 100);
+}
+
 
  
-
-}
-
-function mouseDragged(){
-    ellipse (200, 200, 100, 100);
-    value = value + 5;
-    if (value > 255){
-        vaule = 0;
-    }
-}
 
 function random(){
     for (let i = 0; i < 100; i++) {
@@ -28,8 +21,16 @@ function random(){
       }
 }
 
+function mouseDragged(){
+    random(ellipse (mouseX, mouseY, mouseY, mouseY));
+    value = value + 5;
+    if (value > 255){
+        value = 0;
+    }
+}
+
 function mouseClicked() {
-  if (value <= 0) {
+  if (value === 0) {
     value = 255;
   } else {
     value = 0;
