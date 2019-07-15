3fd8c3d1c3beaceb0b12b84bec1d4907e840dc0f
michaelnASC6 <mnewby0503@gmail.com>
2019-07-15 12:05:26 -0400

    In class; finished

---
+++ day0/p5-intro/intro.js
@@ -0,0 +1,12 @@
+//sets up canvas
+function setup(){
+    createCanvas(500, 500);
+    background(255, 0, 0);
+}
+
+//allows us to work on canvas
+function draw(){
+    // console.log("Nice to say Hello!")
+fill (0);
+    circle(250, 250, 100);
+}
